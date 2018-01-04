21e4acc6854a4af95e6e51af623a4b1c30cf782b
Bastian Schroll <bastian@dieschrollis.de>
2018-01-04 15:51:35 +0100

    add doxygen files
    for windows

---
+++ doxygen.bat
@@ -0,0 +1,18 @@
+@echo off
+
+echo " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " "
+echo "     ____  ____  ______       __      __       __       _____ "
+echo "    / __ )/ __ \/ ___/ |     / /___ _/ /______/ /_     |__  / "
+echo "   / __  / / / /\__ \| | /| / / __ `/ __/ ___/ __ \     /_ <  "
+echo "  / /_/ / /_/ /___/ /| |/ |/ / /_/ / /_/ /__/ / / /   ___/ /  "
+echo " /_____/\____//____/ |__/|__/\__,_/\__/\___/_/ /_/   /____/   "
+echo "                German BOS Information Script                 "
+echo "                     by Bastian Schroll                       "
+echo " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " "
+echo.
+echo Build Doxygen Documentation
+echo.
+_bin\win\doxygen\doxygen.exe "config\doxygen.ini"
+echo.
+echo.
+pause
