92f927532c1cc99ee8ac14b823f7a248e757cda4
Bastian Schroll <bastian@dieschrollis.de>
2018-01-04 18:39:13 +0100

    add cloc tool
    count lines of code

---
+++ clolc.bat
@@ -0,0 +1,18 @@
+@echo off
+
+echo " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " "
+echo "     ____  ____  ______       __      __       __       _____ "
+echo "    / __ )/ __ \/ ___/ |     / /___ _/ /______/ /_     |__  / "
+echo "   / __  / / / /\__ \| | /| / / __ `/ __/ ___/ __ \     /_ <  "
+echo "  / /_/ / /_/ /___/ /| |/ |/ / /_/ / /_/ /__/ / / /   ___/ /  "
+echo " /_____/\____//____/ |__/|__/\__,_/\__/\___/_/ /_/   /____/   "
+echo "                German BOS Information Script                 "
+echo "                     by Bastian Schroll                       "
+echo " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " "
+echo.
+echo Run count lines of code ...
+echo.
+_bin\win\cloc_1_72\cloc-1.72.exe .
+echo.
+echo.
+pause
