44aae57592566e31918e80ab4a2266aad5074394
KoenigMjr <135820716+KoenigMjr@users.noreply.github.com>
2026-07-03 18:05:24 +0200

    [feat/multicast]: refactor(router): implement recursive sub-routing for packet lists
    Introduced a recursive routing mechanism (`_process_route_recursive`) to
    natively handle branching pipelines when a module or plugin returns a
    list of packets (e.g., multicast alarms).
    
    - Replaced the linear loop in `runRouter` with a recursive approach.
    - Added strict state isolation via `copy.deepcopy()` for each branched packet.
    - Maintained 100% backward compatibility for standard single-packet flows.

--- boswatch/router/router.py
+++ boswatch/router/router.py
@@ -10,8 +10,8 @@ r"""!
                      by Bastian Schroll
 
 @file:        router.py
-@date:        01.03.2019
-@author:      Bastian Schroll
+@date:        03.07.2026
+@author:      Bastian Schroll, Claus Schichl
 @description: Class for the BOSWatch packet router
 """
 import logging
@@ -51,32 +51,72 @@ class Router:
         r"""!Run the router
 
         @param bwPacket: instance of Packet class
-        @return a instance of Packet class
+        @return an instance of Packet class, a list of packets, or False
         """
         self._runCount += 1
         tmpTime = time.time()
 
         logging.debug("[%s] started", self.name)
 
-        for routeObject in self.routeList:
-            logging.debug("[%s] -> run route: %s", self.name, routeObject.name)
-            bwPacket_tmp = routeObject.callback(copy.deepcopy(bwPacket))  # copy bwPacket to prevent edit the original
+        # Start processing the route list recursively from index 0
+        final_result = self._process_route_recursive(bwPacket, 0)
 
-            if bwPacket_tmp is None:  # returning None doesnt change the bwPacket
-                continue
-
-            if bwPacket_tmp is False:  # returning False stops the router immediately
-                logging.debug("[%s] stopped", self.name)
-                break
-
-            bwPacket = bwPacket_tmp
-            logging.debug("[%s] bwPacket returned", self.name)
         logging.debug("[%s] finished", self.name)
 
         self._routerTime = time.time() - tmpTime
         self._cumTime += self._routerTime
 
-        return bwPacket
+        return final_result
+
+    def _process_route_recursive(self, bwPacket, start_index):
+        r"""!Recursively process the route to support list branching (e.g. for multicast)
+
+        @param bwPacket: current packet to process
+        @param start_index: index of the routeList to start from
+        @return processed bwPacket, list of packets, or False
+        """
+        current_packet = bwPacket
+
+        for i in range(start_index, len(self.routeList)):
+            routeObject = self.routeList[i]
+            logging.debug("[%s] -> run route: %s", self.name, routeObject.name)
+
+            # State Isolation: pass a deep copy to prevent plugins from corrupting the RAM state of subsequent list items
+            bwPacket_tmp = routeObject.callback(copy.deepcopy(current_packet))
+
+            if bwPacket_tmp is None:
+                # returning None doesnt change the current_packet
+                continue
+
+            if bwPacket_tmp is False:
+                # returning False stops the route immediately for this specific packet branch
+                logging.debug("[%s] stopped at route %s", self.name, routeObject.name)
+                return False
+
+            if isinstance(bwPacket_tmp, list):
+                # Branching logic: A module returned a list of packets
+                logging.debug("[%s] route %s returned a list. Branching for %d packets.", self.name, routeObject.name, len(bwPacket_tmp))
+                results = []
+
+                for single_packet in bwPacket_tmp:
+                    # Recursively process the rest of the route (starting at next index) for each packet
+                    res = self._process_route_recursive(single_packet, i + 1)
+
+                    # Aggregate results
+                    if res is not False and res is not None:
+                        if isinstance(res, list):
+                            results.extend(res)
+                        else:
+                            results.append(res)
+
+                # The recursive calls already finished the rest of the route for all branches.
+                # We return the aggregated results immediately to break out of this current loop level.
+                return results if results else False
+
+            # Normal single packet path: update the packet for the next iteration
+            current_packet = bwPacket_tmp
+
+        return current_packet
 
     def _getStatistics(self):
         r"""!Returns statistical information's from last router run
